43d4255abe38da1fd4935d2a2f8eec86c42b9659
Ezekiel54321 <g.ratorrex54321@gmail.com>
2021-06-22 14:39:44 +0100

    INTENT

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,46 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    var USERNAME : String ="admin"
-    var PASSWORD : String = "admin"
+    val USERNAME : String ="admin"
+    val PASSWORD : String = "admin"
+    private lateinit var usernameInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var submitButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        usernameInput = findViewById(R.id.button)
+        usernameInput = findViewById(R.id.editTextTextPassword2)
+        usernameInput = findViewById(R.id.editTextTextPersonName)
+
+        submitButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            login(username, password)
+
+        }
     }
-}
+
+    fun login(username: String, Password:String){
+        if((isValidCredentials(username, Password))) {
+            val intent: Intent = Intent(this, HomeActivity::class.java)
+        }else{
+                Toast.makeText(applicationContext,"Login unsuccessful", Toast.LENGTH_SHORT)
+            }
+
+        }
+
+    }
+    private fun isValidCredentials(username: String, password: String): Boolean{
+        if(username == username && password == password) return true
+        return false
+    }
